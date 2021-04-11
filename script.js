11fe51374a068b7a86f3745dfb1bdece40e4a540
Nico Carlson <nicocarlson285@gmail.com>
2021-04-11 16:33:20 -0500

    DRY principle applied to dark/light mode functions

--- script.js
+++ script.js
@@ -15,23 +15,32 @@ const imageMode = function(color) {
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Dark Mode  Functions for Switch Theme Function to call
-const darkMode = function() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
+//DRY principle
+const toggleDarkLightMode = function(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');
 };
 
-// Light Mode
-const lightMode =  function() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
-};
+// // Dark Mode  Functions for Switch Theme Function to call
+// const darkMode = function() {
+//     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//     toggleIcon.children[0].textContent = 'Dark Mode';
+//     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+//     imageMode('dark');
+// };
+
+// // Light Mode
+// const lightMode =  function() {
+//     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//     toggleIcon.children[0].textContent = 'Light Mode';
+//     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+//     imageMode('light');
+// };
 
 // Switch Theme function
 const switchTheme = function (event) {
@@ -39,11 +48,11 @@ const switchTheme = function (event) {
       document.documentElement.setAttribute('data-theme', 'dark');
       //Store value in local storage
       localStorage.setItem('theme', 'dark');
-      darkMode();
+      toggleDarkLightMode(true);
   } else {
       document.documentElement.setAttribute('data-theme', 'light');
       localStorage.setItem('theme', 'light')
-      lightMode();
+      toggleDarkLightMode(false);
   }
 };
 
@@ -56,6 +65,6 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
-        darkMode();
+        toggleDarkLightMode(true);
     };
 }
